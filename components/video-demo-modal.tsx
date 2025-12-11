4b3909971840bde4bb92c91d1e5312ced2a70dff
mlobacz-png <mlobacz@spastream.net>
2025-12-11 09:11:42 -0600

    Update video-demo-modal.tsx

--- components/video-demo-modal.tsx
+++ components/video-demo-modal.tsx
@@ -60,7 +60,7 @@ export function VideoDemoModal() {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/oobVQaeFWls"
+              src="https://www.youtube.com/embed/cFDeB0Mx4Ao"
               title="SpaStream Demo"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
